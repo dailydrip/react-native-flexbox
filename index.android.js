d3e96811f04351e633b63f77fdfc49a3d64a21c7
Franze Jr <franzejr@gmail.com>
2016-08-29 09:24:57 -0300

    Implement login screen using flexbox

--- index.android.js
+++ index.android.js
@@ -1,53 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicator,
+  View,
 } from 'react-native';
-
-class flexbox extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  titleArea: {
+    flex: 1,
+    marginTop: 100,
   },
-  instructions: {
+  inputArea: {
+    flex: 3,
+    width: 400,
+    justifyContent: 'center',
+  },
+  buttonsArea: {
+    flex: 3,
+    width: 400,
+    alignItems: 'center',
+  },
+  input: {
+    color: 'white',
+    height: 40,
+    margin: 10,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#3f89e2',
+  },
+  button: {
+    marginTop: 10,
+    padding: 10,
+    width: 200,
+    borderRadius: 10,
+    alignItems: 'center',
+    borderColor: '#fff',
+    backgroundColor: '#3f89e2',
+  },
+  indicator: {
+    marginTop: 10,
+    paddingLeft: 10,
+  },
+  buttonText: {
+    color: '#fff',
   },
 });
+
+class flexbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { animating: false };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.titleArea}>
+          <Text>LOGIN APP</Text>
+        </View>
+
+        <View style={styles.inputArea}>
+          <TextInput
+            style={styles.input}
+            placeholder="Login"
+            onChangeText={(login) => this.setState({ login })}
+          />
+
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            onChangeText={(password) => this.setState({ password })}
+          />
+        </View>
+
+        <View style={styles.buttonsArea}>
+
+          <TouchableHighlight style={styles.button} onPress={this.login}>
+            <Text style={styles.buttonText}>LOGIN</Text>
+          </TouchableHighlight>
+
+          <Text>Forgot your password?</Text>
+          <ActivityIndicator
+            animating={this.state.animating}
+            size="large"
+          />
+
+        </View>
+      </View>
+    );
+  }
+}
 
 AppRegistry.registerComponent('flexbox', () => flexbox);
